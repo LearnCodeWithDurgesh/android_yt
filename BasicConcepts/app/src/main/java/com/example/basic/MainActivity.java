134e85fd9f46eef265e68447b78f6c53407ebb1d
Learn Code With Durgesh Officials <55891143+LearnCodeWithDurgesh@users.noreply.github.com>
2023-10-14 22:42:26 +0530

    added spinner and datepicker for date of birth

--- BasicConcepts/app/src/main/java/com/example/basic/MainActivity.java
+++ BasicConcepts/app/src/main/java/com/example/basic/MainActivity.java
@@ -2,27 +2,45 @@ package com.example.basic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button, clearButton;
 
-    private EditText nameEditText, emailEditText, aboutEditText, passwordEditText;
+    private EditText nameEditText, emailEditText, aboutEditText, passwordEditText, datePickerButton;
 
     private RadioGroup genderGroup;
     private RadioButton femaleRadioButton, maleRadioButton, otherRadioButton;
 
     private CheckBox javaCheck, pythonCheck, androidCheck;
+
+    private String countryNames[] = {
+
+            "INDIA",
+            "AUSTRALIA",
+            "SOUTH AFRICA",
+            "KOREA",
+            "CHINA",
+            "USA"
+    };
+
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +48,55 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //button reference aa gaya hai ...ui button signup
         this.initializeViews();
+        //attaching listener to date picker button
+        datePickerButton.setOnClickListener(view -> {
+//            code ...
+//            Toast.makeText(this, "selecting date", Toast.LENGTH_SHORT).show();
+//            show the date picker
+
+//            ye hamne year month aur date ko nikala hai
+            Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(
+                    this,
+                    (view1, selectedYear, selectedMonth, selectedDay) -> {
+//                        ye call hoga kab jab ham date ko pick kar lenge
+                        datePickerButton.setText(selectedDay + "/" + (selectedMonth + 1) + "/" + selectedYear);
+                        Toast.makeText(this, "year: " + selectedYear + " month: " + selectedMonth + " day : " + selectedDay, Toast.LENGTH_SHORT).show();
+
+                    },
+                    year,
+                    month,
+                    day
+            );
+
+            datePickerDialog.show();
+        });
+
+//        adapter use spinner ko populate karne lie
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, countryNames);
+
+        spinner.setAdapter(arrayAdapter);
+        spinner.setSelection(5);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, countryNames[position] + " selected", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
         //color change
         //text change
@@ -53,6 +120,8 @@ public class MainActivity extends AppCompatActivity {
             String email = emailEditText.getText().toString();
             String password = passwordEditText.getText().toString();
             String about = aboutEditText.getText().toString();
+            String dob = datePickerButton.getText().toString();
+            String country = (String) spinner.getSelectedItem();
 
 
 //            validations
@@ -108,6 +177,8 @@ public class MainActivity extends AppCompatActivity {
             Log.w("Name", name);
             Log.w("Gender", gender);
             Log.w("Courses", courses);
+            Log.w("Country", country);
+
 
             Toast.makeText(this, "Got the data", Toast.LENGTH_SHORT).show();
 
@@ -135,5 +206,7 @@ public class MainActivity extends AppCompatActivity {
         pythonCheck = findViewById(R.id.python_course);
         androidCheck = findViewById(R.id.android_course);
         clearButton = findViewById(R.id.clear_button);
+        datePickerButton = findViewById(R.id.pick_date_button);
+        spinner = findViewById(R.id.country_spinner);
     }
 }
